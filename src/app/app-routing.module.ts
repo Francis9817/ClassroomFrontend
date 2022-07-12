ea23c3a6743c634eb1149243dacd4fde764478f7
Carolina Yuqui <carolinayuqui28@outlook.es>
2022-07-11 21:28:55 -0500

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { subscribeOn } from 'rxjs';
 import { CareerComponent } from './pages/component-career/career/career.component';
 import { ClassroomComponent } from './pages/component-classroom/classroom/classroom.component';
 import { CourseComponent } from './pages/component-course/course/course.component';
 import { DayComponent } from './pages/component-day/day/day.component';
-import { EntitiesComponent } from './pages/component-entities/entities/entities.component';
 import { LevelComponent } from './pages/component-level/level/level.component';
 import { ScheduleComponent } from './pages/component-schedule/schedule/schedule.component';
 import { schoolYearComponent } from './pages/component-school-year/school-year/school-year.component';
@@ -12,34 +12,20 @@ import { StatusComponent } from './pages/component-status/status/status.componen
 import { SubjectComponent } from './pages/component-subject/subject/subject.component';
 import { TeacherComponent } from './pages/component-teacher/teacher/teacher.component';
 import { UserComponent } from './pages/component-user/user/user.component';
-import { WeekdaysComponent } from './pages/component-weekdays/weekdays/weekdays.component';
 
 
 const routes: Routes = [
   {path:'career', component: CareerComponent},
   {path:'teacher', component: TeacherComponent},
-  {path:'pages',
-  loadChildren:() => import('./pages/component-course/course.module').then(m=>m.CourseModule)},
-  {path:'pages',
-  loadChildren:() => import('./pages/component-day/day.module').then(m=>m.DayModule)},
-  {path:'pages',
-  loadChildren:() => import('./pages/component-entities/entities.module').then(m=>m.EntitiesModule)},
-  {path:'pages',
-  loadChildren:() => import('./pages/component-level/level.module').then(m=>m.LevelModule)},
-  {path:'pages',
-  loadChildren:() => import('./pages/component-schedule/schedule.module').then(m=>m.ScheduleModule)},
-  {path:'pages',
-  loadChildren:() => import('./pages/component-school-year/school-year.module').then(m=>m.schoolYearModule)},
-  {path:'pages',
-  loadChildren:() => import('./pages/component-status/status.module').then(m=>m.StatusModule)},
-  {path:'pages',
-  loadChildren:() => import('./pages/component-subject/subject.module').then(m=>m.SubjectModule)},
-  {path:'pages',
-  loadChildren:() => import('./pages/component-teacher/teacher.module').then(m=>m.TeacherModule)},
-  {path:'pages',
-  loadChildren:() => import('./pages/component-user/user.module').then(m=>m.UserModule)},
-  {path:'pages',
-  loadChildren:() => import('./pages/component-weekdays/weekdays.module').then(m=>m.WeekdaysModule)},
+  {path:'course', component: CourseComponent},
+  {path:'day', component: DayComponent},
+  {path:'level', component: LevelComponent},
+  {path:'schedule', component:ScheduleComponent},
+  {path:'school-year', component:schoolYearComponent},
+  {path:'status', component: StatusComponent},
+  {path:'subject', component: SubjectComponent},
+  {path:'user', component: UserComponent},
+  {path:'classroom', component: ClassroomComponent},
 ];
 
 
